cd4f5e8c8438bb7f820531f94809cb56a7604fde
nichol alexander <nichol.alexander@gmail.com>
2018-03-09 09:53:40 -0500

    init and search bar boilerplate

---
+++ src/containers/search_bar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+// class
+
+export default class SearchBar extends Component {
+  render(){
+    return(
+      <form className="input-group">
+        <input />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+// mapStateToProps
+
+// connect
